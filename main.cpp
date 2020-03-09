a30f1639b99cac87d9af42c19d47c6f18c52f5b4
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-03-08 23:07:15 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -94,8 +94,8 @@ int main()
     view_set_bar();
     view_set_faixa(0, qtd - 1);
 
-    // minimum_sort(vet);
-    quick_sort(vet, 0, vet.size());
+    minimum_sort(vet);
+    // quick_sort(vet, 0, vet.size());
     // bubble_sort(vet);
 
     view_lock();
